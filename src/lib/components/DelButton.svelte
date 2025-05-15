213d03cf427265e6e6eb0c546613b8b48d876ea2
AM <dev-charodeyka@proton.me>
2025-05-15 22:16:45 +0200

    build

--- src/lib/components/DelButton.svelte
+++ src/lib/components/DelButton.svelte
@@ -4,11 +4,11 @@
 
 <button
   onclick={handleDelVertex}
-  class="text-aura-purple [&:hover>svg>path:first-child]:text-aura-yellow [&:hover>svg>path:first-child]:stroke-aura-yellow [&:hover>svg>g>path]:text-dark-purple absolute right-1 top-1 h-7 cursor-pointer rounded-full"
+  class="text-aura-purple [&:hover>svg>path:first-child]:text-aura-yellow [&:hover>svg>path:first-child]:stroke-aura-red [&:hover>svg>g>path]:text-dark-purple absolute right-1 top-1 h-6 cursor-pointer rounded-full"
   ><svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"
     ><!-- Icon from Myna UI Icons by Praveen Juge - https://github.com/praveenjuge/mynaui-icons/blob/main/LICENSE -->
     <path
-      class="group-hover:animate-rotate text-vertex-info-bg stroke-aura-purple origin-center fill-current"
+      class="group-hover:animate-rotate text-vertex-info-bg stroke-dark-red origin-center fill-current"
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="1.5"
@@ -17,7 +17,7 @@
     <g transform="translate(12 12) scale(0.6) translate(-12 -12)">
       <!-- Icon from Myna UI Icons by Praveen Juge - https://github.com/praveenjuge/mynaui-icons/blob/main/LICENSE -->
       <path
-        class="stroke-aura-purple fill-current"
+        class="stroke-dark-red fill-current"
         d="M5 12a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1"
       />
     </g></svg
--- src/lib/components/PseudoCode.svelte
+++ src/lib/components/PseudoCode.svelte
@@ -19,23 +19,29 @@
 <div class="grid h-full w-full grid-rows-10 text-xs">
   <div class="justify-left flex h-full w-full items-center text-sm">
     <span>{'> algorithmPCA('}</span>
-    <span class="bg-aura-darkpurple rounded-md px-1 py-0.5">{algInputPlaceholder()}</span>
+    <span class="bg-aura-darkpurple border-aura-green border rounded-md px-1 py-0.5 text-center"
+      >{algInputPlaceholder()}</span
+    >
     <span>{'):'}</span>
   </div>
   {#each traversedPath.slice(1) as step, i (step)}
     <div class="justify-left flex h-full w-full items-center pl-6 text-sm">
-      <span>{vertices.find((vertex) => vertex.id === step)?.output || 'output'}</span>
-      <span class="bg-aura-darkpurple rounded-md px-1 py-0.5"
+      <span class="bg-aura-darkpurple border-aura-green rounded-md border px-1 py-0.5 text-center"
+        >{vertices.find((vertex) => vertex.id === step)?.output || 'output'}</span
+      >
+      {'<-'}<span class=" rounded-md px-1 py-0.5 text-center"
         >{vertices.find((vertex) => vertex.id === step)?.codeName || 'operation'}</span
       >
       <span>{'('}</span>
-      <span>{vertices.find((vertex) => vertex.id === traversedPath[i])?.output || 'Input'}</span>
+      <span class="bg-aura-darkpurple rounded-md px-1 py-0.5 text-center"
+        >{vertices.find((vertex) => vertex.id === traversedPath[i])?.output || 'Input'}</span
+      >
       <span>{')'}</span>
     </div>
   {/each}
   <div class="justify-left flex h-full w-full items-center pl-6 text-base">
     <span class="pr-2">{'return'}</span>
-    <span class="bg-aura-darkpurple rounded-md px-1 py-0.5"
+    <span class="bg-aura-darkpurple  rounded-md px-1 py-0.5 text-center"
       >{vertices.find((vertex) => vertex.id === traversedPath[traversedPath.length - 1])?.output ||
         vertices.find((vertex) => vertex.id === getSelDataVertexId())?.output ||
         'output'}</span
